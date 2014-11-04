799223c442442df685008b3c723563efbf9787f5
Daniel Persson <zirth66@gmail.com>
2014-11-04 22:17:03 +0100

    Map is done

--- Assets/TileMap.cs
+++ Assets/TileMap.cs
@@ -4,9 +4,14 @@ using System.Collections;
 [RequireComponent (typeof(MeshFilter))]
 [RequireComponent (typeof(MeshRenderer))]
 [RequireComponent (typeof(MeshCollider))]
+[ExecuteInEditMode]
 
 public class TileMap : MonoBehaviour {
-	 
+
+	//Number of tiles on map
+	public int size_x = 100;
+	public int size_z = 50;
+	public float tileSize = 1.0f;
 
 	// Use this for initialization
 	void Start () 
@@ -15,53 +20,55 @@ public class TileMap : MonoBehaviour {
 	}
 
 	void BuildMesh()
-	{
+	{	//Vertices always one more than number of tiles
+		int numTiles = size_x * size_z;
+		int numTris = numTiles * 2;
+
+		int vsize_x = size_x + 1;
+		int vsize_z = size_z + 1;
+		int numVerts = vsize_x * vsize_z; 
+
 		// Generate the mesh data
-		Vector3[] vertices = new Vector3[4];
-		int[] triangles = new int[2 * 3];
-		Vector3[] normals = new Vector3[4];
-		Vector2[] uv = new Vector2[4];
+		Vector3[] vertices = new Vector3[numVerts];
+		Vector3[] normals = new Vector3[numVerts];
+		Vector2[] uv = new Vector2[numVerts];
 
-		// ( X, Y, Z)
-		vertices[0] = new Vector3 (0, 0, 0);
-		vertices[1] = new Vector3 (1, 0, 0);
-		vertices[2] = new Vector3 (0, 0, -1);
-		vertices[3] = new Vector3 (1, 0, -1);
-		
+		int[] triangles = new int[numTris * 3];
 
-		/*
-		 * 0	1
-		 * ------
-		 * |\	|
-		 * | \	|
-		 * |  \	|
-		 * ------
-		 * 2	3
-		 * 
-		 */
-		triangles[0] = 0;
-		triangles[1] = 3;
-		triangles[2] = 2;
+		int x;
+		int z;
 
-		triangles[3] = 0;
-		triangles[4] = 1;
-		triangles[5] = 3;
+		for (z=0; z < vsize_z; z++)
+		{
+			for (x=0; x < vsize_x; x++)
+			{
+				vertices[ z * vsize_x + x ] = new Vector3( x*tileSize, 0, z*tileSize );
+				normals[ z * vsize_x + x ] = Vector3.up;
+				uv[ z * vsize_x + x ] = new Vector2( (float)x / vsize_x, (float)z / vsize_z );
+			}
+		}
+		Debug.Log("Done verts!");
 
-		normals[0] = Vector3.up;
-		normals[0] = Vector3.up;
-		normals[0] = Vector3.up;
-		normals[0] = Vector3.up;
+		for (z=0; z < size_z; z++)
+		{
+			for (x=0; x < size_x; x++)
+			{
+				int squareIndex = z * size_x + x; 
+				int triOffset = squareIndex * 6;
+				triangles[triOffset + 0] = z * vsize_x + x + 		   0;
+				triangles[triOffset + 1] = z * vsize_x + x + vsize_x + 0;
+				triangles[triOffset + 2] = z * vsize_x + x + vsize_x + 1;
 
-		uv[0] = new Vector2 (1, 1);
-		uv[1] = new Vector2 (0, 1);
-		uv[2] = new Vector2 (1, 0);
-		uv[3] = new Vector2 (0, 0);
-
-
+				triangles[triOffset + 3] = z * vsize_x + x + 		   0;
+				triangles[triOffset + 4] = z * vsize_x + x + vsize_x + 1;
+				triangles[triOffset + 5] = z * vsize_x + x + 		   1;
+			}
+		}
+		Debug.Log ("Done triangles!");
 
 
 		// Create a new Mesh and populate with the data
-		Mesh mesh = new Mesh ();
+		Mesh mesh = new Mesh();
 		mesh.vertices = vertices;
 		mesh.triangles = triangles;
 		mesh.normals = normals;
@@ -73,6 +80,8 @@ public class TileMap : MonoBehaviour {
 		MeshCollider mesh_colider = GetComponent<MeshCollider> ();
 
 		mesh_filter.mesh = mesh;
+		Debug.Log ("Done mesh!");
+
 	}
 
 }
